45cd7af732b4117084ea4431fe17272bf8ea49b0
Thaung Htike Oo <thaunghtikeoo@Thaungs-MacBook-Air.local>
2022-05-18 22:45:28 +0630

    update tf code

--- lambda.tf
+++ lambda.tf
@@ -44,9 +44,9 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
   policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
 }
 
-resource "aws_lambda_function" "terraform_lambda_func" {
+resource "aws_lambda_function" "rds_cross_region_backup" {
   filename      = "${path.module}/app.py"
-  function_name = "RDS_Cross_Region_Backup_Function"
+  function_name = "RDSCrossRegionBackup-${var.source_region}-${var.target_region}-${join("-", var.instances)}"
   role          = aws_iam_role.lambda_role.arn
   handler       = "index.lambda_handler"
   runtime       = "python3.9"
