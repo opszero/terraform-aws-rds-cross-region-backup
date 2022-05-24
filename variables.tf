0f4401a7dcd7cba8dfbe2e7d6c35a8f630abd530
thaunghtike-share <87596555+thaunghtike-share@users.noreply.github.com>
2022-05-24 09:15:55 +0630

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,19 @@
-variable "source_region" {}
-variable "target_region" {}
-variable "instances" {}
-variable "account" {}
+variable "source_region" {
+  description = "The AWS region where the source RDS instance exists"
+}
+
+variable "target_region" {
+  description = "The AWS region where you want to copy RDS backups"
+}
+
+variable "instances" {
+  description = "List of RDS instances"
+}
+
+variable "account" {
+  description = "AWS Account ID"
+}
+
+variable "kms_key_id" {
+  description = "The ID of KMS key"
+}
